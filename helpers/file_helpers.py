e240d492ab2d412d8cc35764a00c4e0459c84325
catareias <katyareias@hotmail.com>
2024-12-27 16:39:18 +0000

    day 2 part 1 complete

--- helpers/file_helpers.py
+++ helpers/file_helpers.py
@@ -12,6 +12,10 @@ def split_rows_into_columns(rows):
         list2.append(int(columns[-1]))
     return  list1, list2
 
+def split_rows_into_values(rows):
+    values = [value.split(' ') for value in rows]
+    return values
+
 def export_columns_from_file(filename):
     rows = export_rows_from_file(filename)
     return split_rows_into_columns(rows)
